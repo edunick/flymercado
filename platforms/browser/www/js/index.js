18926742fcbb75da7ed5d235f6c291f015297bd4
Eduardo Cortez <eduardo@isourcedigitalcom>
2017-04-19 15:30:06 -0300

    update

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -27,7 +27,7 @@ var app = {
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
-        var ref = window.open('http://www.flymercado.com', '_self');
+        
 
     },
     // deviceready Event Handler
