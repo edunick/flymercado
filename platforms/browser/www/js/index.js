85de3311a0faac14be47c65f9865ffd9a67240b9
Eduardo Cortez <eduardo@isourcedigitalcom>
2017-04-20 12:28:57 -0300

    test

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -35,6 +35,8 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+         var url = 'http://www.flymercado.com' // change to whatever you want
+        cordova.InAppBrowser.open(url, '_self', 'location=no');
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
